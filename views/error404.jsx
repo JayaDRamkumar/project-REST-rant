719f5851d4aaec4907fae7404d62dc35d86ad254
Jaya Ramkumar <jayaramkumar@MacBook-Pro.fios-router.home>
2023-07-09 14:35:59 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,14 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                  <img src="/images/dog.jpg" alt="Variety Fruit" />
+                 <div>
+                  Photo by <a href="https://unsplash.com/pt-br/@mtsjrdl?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mathis Jrdl</a> on <a href="https://unsplash.com/images/animals/puppies?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+                 </div>
+                 </div>
+                  <div></div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -3,13 +3,22 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/variety-fruit.jpg" alt="Variety Fruit" />
+                  <div>
+                  Photo by <a href="https://unsplash.com/@jimmydean?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jimmy Dean</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                  </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
+
