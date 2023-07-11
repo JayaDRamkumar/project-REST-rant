c0bc9fb106158fb9b71d3996f654833fff6326e2
Jaya Ramkumar <jayaramkumar@MacBook-Pro.fios-router.home>
2023-07-11 19:48:14 -0400

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -17,7 +17,7 @@ router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = '/images/unknown.jpg'
   }
   if (!req.body.city) {
     req.body.city = 'Anytown'
